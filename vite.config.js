95f553784a6001a808247e9caa320387c60d8c81
Anuj Tripathi <149668092+anuj452005@users.noreply.github.com>
2025-07-02 22:22:06 +0530

    added vite.config.js and renamed all .js to .jsx

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.jsx'),
+      name: 'OpenIMISFeInsuree',
+      fileName: (format) => `index.${format === 'es' ? 'es.' : ''}js`,
+      formats: ['es', 'cjs'],
+    },
+    rollupOptions: {
+      external: [
+        /^@babel.*/,
+        /^@date-io\/.*/,
+        /^@material-ui\/.*/,
+        /^@openimis.*/,
+        'classnames',
+        'clsx',
+        'history',
+        /^lodash.*/,
+        'moment',
+        'prop-types',
+        /^react.*/,
+        /^redux.*/
+      ],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+    sourcemap: true,
+    outDir: 'dist',
+    emptyOutDir: false,
+  },
+});
